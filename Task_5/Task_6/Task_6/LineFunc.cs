965b45cd957b9d1482c37f07ba12bd817992313c
zzZMortganZzz <mikhaylov-kg@mail.ru>
2023-09-04 08:09:47 +1000

    HomeWork_6

---
+++ Task_5/Task_6/Task_6/LineFunc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    public class LineFunc
+    {
+        public int K { get; set; }
+        public int B { get; set; }
+
+        public override string ToString()
+        {
+            return  String.Format("Y ={0}X+{1}",K,B);
+            //return base.ToString();
+        }
+
+        public static Point CrossPoint(LineFunc _ln1, LineFunc _ln2)
+        {
+
+            Point _p = new Point();
+            _p.X = (_ln1.B - _ln2.B) / (_ln1.K - _ln2.K);
+            _p.X = _ln1.K * _p.X + _ln1.B;
+            return _p;
+        }
+
+
+    }
+
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("({0};{1})", X, Y);
+        }
+
+       
+
+    }
+}
---
+++ Task_5/Task_6/Task_6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Задание 1
+            int count = Convert.ToInt32(Console.ReadLine());
+            int countItem = 0;
+            for (int i = 0; i < count; i++)
+            {
+                int tmp = Convert.ToInt32(Console.ReadLine());
+                int val = tmp > 0 ? 1 : 0;
+                countItem = countItem + val;
+            }
+            Console.WriteLine(countItem);
+            Console.ReadKey();
+            #endregion
+
+
+            #region Задание 2
+            LineFunc _ln1 = new LineFunc() { K = -2, B = 6 };
+            LineFunc _ln2 = new LineFunc() { K = 6, B = 1 };
+
+            Point _p = LineFunc.CrossPoint(_ln1, _ln2);
+
+            Console.WriteLine("Точка пересечения функций {0} и {1}: {2}", _ln1.ToString(), _ln2.ToString(), _p.ToString());
+            Console.ReadKey();
+
+            #endregion
+        }
+    }
+}
